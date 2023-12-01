36bc7abe3c0dcd8c50e4702fdf6bb988951ea227
David Waite <daveycrockett02@gmail.com>
2023-12-01 14:06:39 -0700

    add check and x for C/IC, enabled drop down to update question list

--- Components/Pages/Question.cs
+++ Components/Pages/Question.cs
@@ -1,8 +1,14 @@
-﻿namespace Quizzing.Components.Pages
+﻿using static System.Formats.Asn1.AsnWriter;
+using System.Xml.Linq;
+
+namespace Quizzing.Components.Pages
 {
-	public class Question(QuestionType type, int pointAmt)
+	public class Question()
 	{
-		private QuestionType type = type;
-		private int pointAmt = pointAmt;
+		private QuestionType type = QuestionType.Speed;
+		private int pointAmt = 0;
+
+		public QuestionType Type { get { return type; } set { type = value; } }
+		public int PointAmt { get { return pointAmt; } set { pointAmt = value; } }
 	}
 }
